701755ada64b123e6f6cb828d33da9d34dae1cc4
luyiqiao <1602453396@qq.com>
2017-12-17 17:04:13 +0800

    更新

--- ajax/comment.php
+++ ajax/comment.php
@@ -19,7 +19,22 @@ if (empty($post->comment_status)) {
 $status = get_post_status($post);
 $status_obj = get_post_status_object($status);
 
-do_action('pre_comment_on_post', $comment_post_ID);
+
+if ( !comments_open($comment_post_ID) ) {
+    do_action('comment_closed', $comment_post_ID);
+    err(__('Sorry, comments are closed for this item.', 'Nana')); // 将 wp_die 改为错误提示
+} elseif ( 'trash' == $status ) {
+    do_action('comment_on_trash', $comment_post_ID);
+    err(__('Invalid comment status.', 'Nana')); // 将 exit 改为错误提示
+} elseif ( !$status_obj->public && !$status_obj->private ) {
+    do_action('comment_on_draft', $comment_post_ID);
+    err(__('Invalid comment status.', 'Nana')); // 将 exit 改为错误提示
+} elseif ( post_password_required($comment_post_ID) ) {
+    do_action('comment_on_password_protected', $comment_post_ID);
+    err(__('Password Protected', 'Nana')); // 将 exit 改为错误提示
+} else {
+    do_action('pre_comment_on_post', $comment_post_ID);
+}
 
 $comment_author = (isset($_POST['author'])) ? trim(strip_tags($_POST['author'])) : null;
 $comment_author_email = (isset($_POST['email'])) ? trim($_POST['email']) : null;
@@ -28,31 +43,35 @@ $comment_content = (isset($_POST['comment'])) ? trim($_POST['comment']) : null;
 $edit_id = (isset($_POST['edit_id'])) ? $_POST['edit_id'] : null; // 提取 edit_id
 // If the user is logged in
 $user = wp_get_current_user();
-if ($user->ID) {
-    if (empty($user->display_name))
-        $user->display_name = $user->user_login;
-    $comment_author = $wpdb->escape($user->display_name);
+// If the user is logged in
+$user = wp_get_current_user();
+if ( $user->ID ) {
+    if ( empty( $user->display_name ) )
+        $user->display_name=$user->user_login;
+    $comment_author       = $wpdb->escape($user->display_name);
     $comment_author_email = $wpdb->escape($user->user_email);
-    $comment_author_url = $wpdb->escape($user->user_url);
-    if (current_user_can('unfiltered_html')) {
-        if (wp_create_nonce('unfiltered-html-comment_' . $comment_post_ID) != $_POST['_wp_unfiltered_html_comment']) {
+    $comment_author_url   = $wpdb->escape($user->user_url);
+    if ( current_user_can('unfiltered_html') ) {
+        if ( wp_create_nonce('unfiltered-html-comment_' . $comment_post_ID) != $_POST['_wp_unfiltered_html_comment'] ) {
             kses_remove_filters(); // start with a clean slate
             kses_init_filters(); // set up the filters
         }
     }
 } else {
-    if (get_option('comment_registration') || 'private' == $status)
-        err('Hi，你必须登录才能发表评论！'); // 將 wp_die 改為錯誤提示
+    if ( get_option('comment_registration') || 'private' == $status )
+        err(__('对不起，您必须登录后才能发表评论。', 'Nana')); // 将 wp_die 改为错误提示
 }
+
 $comment_type = '';
-if (get_option('require_name_email') && !$user->ID) {
+if (get_my_option('require_name_email') && !$user->ID) {
     if (6 > strlen($comment_author_email) || '' == $comment_author)
         err('请填写昵称和邮箱！'); // 將 wp_die 改為錯誤提示
     elseif (!is_email($comment_author_email))
         err('请填写有效的邮箱地址！'); // 將 wp_die 改為錯誤提示
 }
 if ('' == $comment_content)
-    err('请填写点评论！'); // 將 wp_die 改為錯誤提示
+    err('请输入评论内容！'); // 將 wp_die 改為錯誤提示
+
 // 增加: 錯誤提示功能
 function err($ErrMsg)
 {
@@ -61,8 +80,28 @@ function err($ErrMsg)
     exit;
 }
 
+
+// 检查重复评论功能
+$dupe = "SELECT comment_ID FROM $wpdb->comments WHERE comment_post_ID = '$comment_post_ID' AND ( comment_author = '$comment_author' ";
+if ( $comment_author_email ) $dupe .= "OR comment_author_email = '$comment_author_email' ";
+$dupe .= ") AND comment_content = '$comment_content' LIMIT 1";
+if ( $wpdb->get_var($dupe) ) {
+    err(__('您似乎已经提交过这条评论了！'));
+}
+
+// 检查评论太快功能
+if ( $lasttime = $wpdb->get_var( $wpdb->prepare("SELECT comment_date_gmt FROM $wpdb->comments WHERE comment_author = %s ORDER BY comment_date DESC LIMIT 1", $comment_author) ) ) {
+    $time_lastcomment = mysql2date('U', $lasttime, false);
+    $time_newcomment  = mysql2date('U', current_time('mysql', 1), false);
+    $flood_die = apply_filters('comment_flood_filter', false, $time_lastcomment, $time_newcomment);
+    if ( $flood_die ) {
+        err(__('您发表评论也太快了'));
+    }
+}
+
 $comment_parent = isset($_POST['comment_parent']) ? absint($_POST['comment_parent']) : 0;
 $commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_type', 'comment_parent', 'user_ID');
+
 // 增加: 檢查評論是否正被編輯, 更新或新建評論
 if ($edit_id) {
     $comment_id = $commentdata['comment_ID'] = $edit_id;
